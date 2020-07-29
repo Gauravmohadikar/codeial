2ea2d7743366d0be0d2d07a2d07bc63643d4601e
Gaurav mohadikar <mohadikar.gaurav@gmail.com>
2020-07-29 16:09:41 +0530

    Seconf Commit: add a Controller and a Route for homer

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+    module.exports.home = function(req, res){
+        return res.end("<h1>Express Server is up for Codieal</h1>")
+    }
+
+    module.exports.play = function(req, res){
+        return res.end("<h1>I guess I'm just a Play Date to you</h1>")
+    }
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const router = express.Router()
+
+const homeController = require("../controllers/home_controller")
+console.log("Routers loaded")
+
+router.get("/", homeController.home)
+router.get("/play", homeController.play)
+
+
+module.exports = router ;
