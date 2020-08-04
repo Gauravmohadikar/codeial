9fe862ed36572cf0d3624b7b5ee19ee86ebf1beb
Gaurav mohadikar <mohadikar.gaurav@gmail.com>
2020-08-05 01:16:23 +0530

    Again commit SignUp / SignIn

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,9 +2,6 @@ const User = require("../models/user");
 
 module.exports.profile = function(req, res){
     // return res.end("<h1>This is User's Profile</h1>")
-    // return res.render("users",{
-    //     title:"localhost:8000/users/profile"
-    // })
 
     if(req.cookies.user_id){
         User.findById(req.cookies.user_id, function(error, user){
